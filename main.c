4a0bee3354b4cb187390925a99844c20e9065666
TGeri <tomcsanyigergely@gmail.com>
2023-05-29 23:59:11 +0200

    Error messages added.

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ bool endsWith(std::string const &str, std::string const &suffix) {
 int main(int argc, char** argv)
 {
     if (argc != 3) {
+        printf("Usage: parser [-caff | -ciff] path-to-file \n");
         return -1;
     }
 
@@ -24,6 +25,7 @@ int main(int argc, char** argv)
     {
         parser::CAFF caff;
         if (!parser::parseCaffFile(filePath, caff)) {
+            printf("Failed to parse CAFF file.\n");
             return -1;
         }
 
@@ -31,17 +33,20 @@ int main(int argc, char** argv)
         filePath = filePath + ".jpg";
 
         if (caff.animations[0].ciff.width > INT_MAX || caff.animations[0].ciff.height > INT_MAX) {
+            printf("Error while saving JPG: First CIFF image size too large.\n");
             return -1;
         }
 
         if (!jpge::compress_image_to_jpeg_file(filePath.c_str(), (int)caff.animations[0].ciff.width, (int)caff.animations[0].ciff.height, 3, (jpge::uint8*)(caff.animations[0].ciff.pixels.data()))) {
-			return -1;
+            printf("Unexpected error while saving first CIFF image as JPG.\n");
+            return -1;
 		}
     }
     else if (fileType == "-ciff" && endsWith(filePath, ".ciff"))
     {
         parser::CIFF ciff;
         if (!parser::parseCiffFile(filePath, ciff)) {
+            printf("Failed to parse CIFF file.\n");
             return -1;
         }
 
@@ -49,15 +54,18 @@ int main(int argc, char** argv)
         filePath = filePath + ".jpg";
 
         if (ciff.width > INT_MAX || ciff.height > INT_MAX) {
+            printf("Error while saving JPG: CIFF image size too large.\n");
             return -1;
         }
 
         if (!jpge::compress_image_to_jpeg_file(filePath.c_str(), (int)ciff.width, (int)ciff.height, 3, (jpge::uint8*)(ciff.pixels.data()))) {
-			return -1;
+            printf("Unexpected error while saving CIFF image as JPG.\n");
+            return -1;
 		}
     }
     else
     {
+        printf("Usage: parser [-caff | -ciff] path-to-file \n");
         return -1;
     }
 
--- parser.c
+++ parser.c
@@ -36,32 +36,39 @@ namespace parser {
         uint64_t startingPos = pos;
 
         if (!datacopy(ciff.magic, buffer, pos, sizeof(ciff.magic))) {
+            printf("Failed to read CIFF magic.\n");
             return false;
         }
 
         if (ciff.magic[0] != 'C' || ciff.magic[1] != 'I' || ciff.magic[2] != 'F' || ciff.magic[3] != 'F') {
+            printf("Invalid CIFF magic.\n");
             return false;
         }
 
         if (!datacopy(&ciff.header_size, buffer, pos, sizeof(ciff.header_size))) {
+            printf("Failed to read CIFF header_size.\n");
             return false;
         }
 
         if (ciff.header_size <
             sizeof(ciff.magic) + sizeof(ciff.header_size) + sizeof(ciff.content_size) + sizeof(ciff.width) +
             sizeof(ciff.height)) {
+            printf("Invalid CIFF header_size.\n");
             return false;
         }
 
         if (!datacopy(&ciff.content_size, buffer, pos, sizeof(ciff.content_size))) {
+            printf("Failed to read CIFF content_size.\n");
             return false;
         }
 
         if (!datacopy(&ciff.width, buffer, pos, sizeof(ciff.width))) {
+            printf("Failed to read CIFF width.\n");
             return false;
         }
 
         if (!datacopy(&ciff.height, buffer, pos, sizeof(ciff.height))) {
+            printf("Failed to read CIFF height.\n");
             return false;
         }
 
@@ -71,22 +78,26 @@ namespace parser {
             ciff.width * ciff.height > UINT64_MAX / 3 ||
             ciff.width > UINT64_MAX / ciff.height / 3 ||
             ciff.height > UINT64_MAX / ciff.width / 3) {
+            printf("Invalid CIFF content_size.\n");
             return false;
         }
 
         pos = startingPos;
 
         if (!datacopy(nullptr, buffer, pos, ciff.header_size)) {
+            printf("Unexpected error while parsing CIFF.\n");
             return false;
         }
 
         if (ciff.content_size > SIZE_MAX) {
+            printf("CIFF content_size is too large.\n");
             return false;
         }
 
         ciff.pixels.resize(ciff.content_size);
 
         if (!datacopy(ciff.pixels.data(), buffer, pos, ciff.content_size)) {
+            printf("Error while parsing CIFF pixels.\n");
             return false;
         }
 
@@ -97,34 +108,41 @@ namespace parser {
         uint64_t startingPos = pos;
 
         if (!datacopy(caffHeader.magic, buffer, pos, sizeof(caffHeader.magic))) {
+            printf("Failed to read magic in CAFF header.\n");
             return false;
         }
 
         if (caffHeader.magic[0] != 'C' || caffHeader.magic[1] != 'A' || caffHeader.magic[2] != 'F' ||
             caffHeader.magic[3] != 'F') {
+            printf("Invalid magic in CAFF header.\n");
             return false;
         }
 
         if (!datacopy(&caffHeader.header_size, buffer, pos, sizeof(caffHeader.header_size))) {
+            printf("Failed to read header_size in CAFF header.\n");
             return false;
         }
 
         if (caffHeader.header_size !=
             sizeof(caffHeader.magic) + sizeof(caffHeader.header_size) + sizeof(caffHeader.num_anim)) {
+            printf("Invalid header_size in CAFF header.\n");
             return false;
         }
 
         if (blockLength != caffHeader.header_size) {
+            printf("CAFF header block length differs from header_size.\n");
             return false;
         }
 
         if (!datacopy(&caffHeader.num_anim, buffer, pos, sizeof(caffHeader.num_anim))) {
+            printf("Failed to read num_anim in CAFF header.\n");
             return false;
         }
 
         pos = startingPos;
 
         if (!datacopy(nullptr, buffer, pos, blockLength)) {
+            printf("Unexpected error while parsing CAFF header.\n");
             return false;
         }
 
@@ -135,28 +153,34 @@ namespace parser {
         uint64_t startingPos = pos;
 
         if (!datacopy(&caffCredits.year, buffer, pos, sizeof(caffCredits.year))) {
+            printf("Failed to read year in CAFF credits.\n");
             return false;
         }
 
         if (!datacopy(&caffCredits.month, buffer, pos, sizeof(caffCredits.month))) {
+            printf("Failed to read month in CAFF credits.\n");
             return false;
         }
 
         if (!datacopy(&caffCredits.day, buffer, pos, sizeof(caffCredits.day))) {
+            printf("Failed to read day in CAFF credits.\n");
             return false;
         }
 
         if (!datacopy(&caffCredits.hour, buffer, pos, sizeof(caffCredits.hour))) {
+            printf("Failed to read hour in CAFF credits.\n");
             return false;
         }
 
         if (!datacopy(&caffCredits.minute, buffer, pos, sizeof(caffCredits.minute))) {
+            printf("Failed to read minute in CAFF credits.\n");
             return false;
         }
 
         uint64_t creator_len;
 
         if (!datacopy(&creator_len, buffer, pos, sizeof(creator_len))) {
+            printf("Failed to read creator_len in CAFF credits.\n");
             return false;
         }
 
@@ -168,20 +192,24 @@ namespace parser {
                                         sizeof(creator_len);
 
         if (creator_len > UINT64_MAX - caffCreditsFixedPartSize) {
+            printf("Invalid creator_len in CAFF credits (too large).\n");
             return false;
         }
 
         if (caffCreditsFixedPartSize + creator_len != blockLength) {
+            printf("CAFF credits block length differs from the size of its content.\n");
             return false;
         }
 
         if (creator_len > SIZE_MAX - 1) {
+            printf("Invalid creator_len in CAFF credits (too large).\n");
             return false;
         }
 
         caffCredits.creator.resize(creator_len + 1);
 
         if (!datacopy((void *) caffCredits.creator.data(), buffer, pos, creator_len)) {
+            printf("Failed to read creator in CAFF credits.\n");
             return false;
         }
 
@@ -190,6 +218,7 @@ namespace parser {
         pos = startingPos;
 
         if (!datacopy(nullptr, buffer, pos, blockLength)) {
+            printf("Unexpected error while parsing CAFF credits.\n");
             return false;
         }
 
@@ -200,16 +229,19 @@ namespace parser {
         uint64_t startingPos = pos;
 
         if (!datacopy(&caffAnimation.duration, buffer, pos, sizeof(caffAnimation.duration))) {
+            printf("Failed to read duration in CAFF animation.\n");
             return false;
         }
 
         if (!parseCiff(buffer, pos, caffAnimation.ciff)) {
+            printf("Failed to parse CIFF in CAFF animation.\n");
             return false;
         }
 
         pos = startingPos;
 
         if (!datacopy(nullptr, buffer, pos, blockLength)) {
+            printf("Unexpected error while parsing CAFF animation.\n");
             return false;
         }
 
@@ -221,6 +253,7 @@ namespace parser {
         file.open(filePath, std::ifstream::in | std::ifstream::binary);
 
         if (!file) {
+            printf("Failed to open CAFF file.\n");
             return false;
         }
 
@@ -234,31 +267,38 @@ namespace parser {
         uint64_t blockLength;
 
         if (!datacopy(&id, buffer, pos, sizeof(id))) {
+            printf("Failed to read first block ID in CAFF file.\n");
             return false;
         }
 
         if (!datacopy(&blockLength, buffer, pos, sizeof(blockLength))) {
+            printf("Failed to read first block length in CAFF file.\n");
             return false;
         }
 
         if (id != 0x1) {
+            printf("Invalid first block ID in CAFF file (must be 0x1).\n");
             return false;
         }
 
         if (!parseCaffHeader(buffer, blockLength, pos, caff.header)) {
+            printf("Failed to parse CAFF header in CAFF file.\n");
             return false;
         }
 
         if (!datacopy(&id, buffer, pos, sizeof(id))) {
+            printf("Failed to read second block ID in CAFF file.\n");
             return false;
         }
 
         if (!datacopy(&blockLength, buffer, pos, sizeof(blockLength))) {
+            printf("Failed to read second block length in CAFF file.\n");
             return false;
         }
 
         if (id == 0x2) {
             if (!parseCaffCredits(buffer, blockLength, pos, caff.credits)) {
+                printf("Failed to parse CAFF credits in CAFF file.\n");
                 return false;
             }
         } else {
@@ -267,20 +307,24 @@ namespace parser {
 
         for (uint64_t i = 0; i < caff.header.num_anim; i++) {
             if (!datacopy(&id, buffer, pos, sizeof(id))) {
+                printf("Failed to read animation block ID in CAFF file.\n");
                 return false;
             }
 
             if (!datacopy(&blockLength, buffer, pos, sizeof(blockLength))) {
+                printf("Failed to read animation block length in CAFF file.\n");
                 return false;
             }
 
             if (id != 0x3) {
+                printf("Invalid animation block ID in CAFF file (must be 0x3).\n");
                 return false;
             }
 
             CAFF_ANIMATION caffAnimation;
 
             if (!parseCaffAnimation(buffer, blockLength, pos, caffAnimation)) {
+                printf("Failed to parse CAFF animation in CAFF file.\n");
                 return false;
             }
 
@@ -295,6 +339,7 @@ namespace parser {
         file.open(filePath, std::ifstream::in | std::ifstream::binary);
 
         if (!file) {
+            printf("Failed to open CIFF file.\n");
             return false;
         }
 
@@ -305,6 +350,7 @@ namespace parser {
         uint64_t pos = 0;
 
         if (!parseCiff(buffer, pos, ciff)) {
+            printf("Failed to parse CIFF file content.\n");
             return false;
         }
 
